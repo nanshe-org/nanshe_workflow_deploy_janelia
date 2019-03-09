d43be6a95354256c0ab0374572cae5208b641239
John Kirkham <kirkhamj@janelia.hhmi.org>
2019-03-09 17:37:53 -0500

    Double quote all paths
    Ensure all paths are double quoted to ensure they are handled correctly.

--- 1_preinstall_nanshe_workflow.sh
+++ 1_preinstall_nanshe_workflow.sh
@@ -42,18 +42,18 @@ fi
 rm -f ~/.nanshe_workflow.sh
 touch ~/.nanshe_workflow.sh
 echo '# Set the temporary directory.' >> ~/.nanshe_workflow.sh
-echo 'TMPDIR=$HOME/tmp' >> ~/.nanshe_workflow.sh
-echo 'TEMP=$TMPDIR' >> ~/.nanshe_workflow.sh
-echo 'TMP=$TMPDIR' >> ~/.nanshe_workflow.sh
+echo 'TMPDIR="$HOME/tmp"' >> ~/.nanshe_workflow.sh
+echo 'TEMP="$TMPDIR"' >> ~/.nanshe_workflow.sh
+echo 'TMP="$TMPDIR"' >> ~/.nanshe_workflow.sh
 echo 'export TMPDIR TEMP TMP' >> ~/.nanshe_workflow.sh
-echo 'mkdir -p $TMPDIR' >> ~/.nanshe_workflow.sh
+echo 'mkdir -p "$TMPDIR"' >> ~/.nanshe_workflow.sh
 echo '' >> ~/.nanshe_workflow.sh
 echo '# Export Grid Engine and DRMAA variables, if available.' >> ~/.nanshe_workflow.sh
 echo '# May not be available when using Linux locally or Windows with Git Bash.' >> ~/.nanshe_workflow.sh
 echo 'if [[ -f /sge/current/default/common/settings.sh ]]; then' >> ~/.nanshe_workflow.sh
 echo '    source /sge/current/default/common/settings.sh' >> ~/.nanshe_workflow.sh
-echo '    export SGE_DRMAA_LIBRARY_PATH=$SGE_ROOT/lib/lx-amd64/libdrmaa.so.1.0' >> ~/.nanshe_workflow.sh
-echo '    export DRMAA_LIBRARY_PATH=$SGE_DRMAA_LIBRARY_PATH' >> ~/.nanshe_workflow.sh
+echo '    export SGE_DRMAA_LIBRARY_PATH="$SGE_ROOT/lib/lx-amd64/libdrmaa.so.1.0"' >> ~/.nanshe_workflow.sh
+echo '    export DRMAA_LIBRARY_PATH="$SGE_DRMAA_LIBRARY_PATH"' >> ~/.nanshe_workflow.sh
 echo 'fi' >> ~/.nanshe_workflow.sh
 echo '# Export LSF variables, if available.' >> ~/.nanshe_workflow.sh
 echo '# May not be available when using Linux locally or Windows with Git Bash.' >> ~/.nanshe_workflow.sh
@@ -61,8 +61,8 @@ echo 'if [[ -f /misc/lsf/conf/profile.lsf ]]; then' >> ~/.nanshe_workflow.sh
 echo '    source /misc/lsf/conf/profile.lsf' >> ~/.nanshe_workflow.sh
 echo '    export LSB_STDOUT_DIRECT='Y'' >> ~/.nanshe_workflow.sh
 echo '    export LSB_JOB_REPORT_MAIL='N'' >> ~/.nanshe_workflow.sh
-echo '    export LSF_DRMAA_LIBRARY_PATH=/misc/sc/lsf-glibc2.3/lib/libdrmaa.so.0.1.1' >> ~/.nanshe_workflow.sh
-echo '    export DRMAA_LIBRARY_PATH=$LSF_DRMAA_LIBRARY_PATH' >> ~/.nanshe_workflow.sh
+echo '    export LSF_DRMAA_LIBRARY_PATH="/misc/sc/lsf-glibc2.3/lib/libdrmaa.so.0.1.1"' >> ~/.nanshe_workflow.sh
+echo '    export DRMAA_LIBRARY_PATH="$LSF_DRMAA_LIBRARY_PATH"' >> ~/.nanshe_workflow.sh
 echo 'fi' >> ~/.nanshe_workflow.sh
 echo '' >> ~/.nanshe_workflow.sh
 echo '# Set the number of OpenBLAS threads to 1.' >> ~/.nanshe_workflow.sh
@@ -76,4 +76,4 @@ echo '# Set the Jupyter runtime directory in the user's home.' >> ~/.nanshe_work
 echo '# This simply follows the recommendation of our cluster admins' >> ~/.nanshe_workflow.sh
 echo '# to redirect this to a different location than XDG_RUNTIME_DIR.' >> ~/.nanshe_workflow.sh
 echo '# This is just what Jupyter picks when XDG_RUNTIME_DIR is disabled.' >> ~/.nanshe_workflow.sh
-echo 'export JUPYTER_RUNTIME_DIR=$HOME/.local/share/jupyter/runtime' >> ~/.nanshe_workflow.sh
+echo 'export JUPYTER_RUNTIME_DIR="$HOME/.local/share/jupyter/runtime"' >> ~/.nanshe_workflow.sh
